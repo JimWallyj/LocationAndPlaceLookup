1e74a39dacdd45d6a4fb468b0b69518decfb3bc9
JimWallyj <jwalejko@me.com>
2026-07-01 20:18:36 -0700

    Initial Commit

---
+++ LocationAndPlaceLookup/LocationAndPlaceLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationAndPlaceLookupApp.swift
+//  LocationAndPlaceLookup
+//
+//  Created by JIM WALEJKO on 7/1/26.
+//
+
+import SwiftUI
+
+@main
+struct LocationAndPlaceLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
